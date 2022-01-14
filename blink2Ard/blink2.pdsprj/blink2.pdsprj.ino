2337532730cecac367cd311d82516213492b2d77
SayanDasgupta <sayan.dasgupta.2000@gmail.com>
2022-01-14 16:36:52 +0530

    count show

--- blink2Ard/blink2.pdsprj/blink2.pdsprj.ino
+++ blink2Ard/blink2.pdsprj/blink2.pdsprj.ino
@@ -2,6 +2,8 @@
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+int count;
+int flag;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,6 +11,8 @@ void setup() {
   pinMode(A0,INPUT);
   pinMode(4,INPUT_PULLUP);
   pinMode(7, OUTPUT);
+  count = 0;
+  flag=0;
 }
 
 int readPin(int pn){
@@ -26,14 +30,28 @@ void loop() {
   lcd.setCursor(10, 0);
   lcd.print(analogRead(A0));
   Serial.println(analogRead(A0));
+
+  lcd.setCursor(0, 1);
+  lcd.print("Count : ");
+  lcd.setCursor(9, 1);
   
   // put your main code here, to run repeatedly:
-  if(readPin(4)==LOW ||  analogRead(A0)>500){
-    setPin(7,HIGH);
-    delay(1000);
-    setPin(7,LOW);
-    delay(1000);
-  }else{
-    setPin(7,LOW);
+//  if(readPin(4)==LOW ||  analogRead(A0)>500){
+//    setPin(7,HIGH);
+//    delay(1000);
+//    setPin(7,LOW);
+//    delay(1000);
+//  }else{
+//    setPin(7,LOW);
+//  }
+  if(analogRead(A0)>500 && flag==0){
+    setPin(7, HIGH);
+    count=count+1;
+    lcd.setCursor(9, 1);
+    lcd.print(count);
+    flag=1;
+  } else if(analogRead(A0)<500 && flag==1){
+    setPin(7, LOW);
+    flag=0;
   }
 }
